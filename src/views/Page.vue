eda990785e55b4add9ec991ae5de85c86d7d7cdd
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2025-10-23 00:22:27 +0800

    Create Page.vue

---
+++ src/views/Page.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page',
+  components: {
+  },
+  data(){
+    return{
+      
+    }
+  },
+  methods:{
+    
+  }
+}
+</script>
+<style>
+
+</style>
