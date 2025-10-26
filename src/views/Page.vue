7c1bedb5864fd63b5d1bb13b459bff67be55e290
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2025-10-27 02:41:27 +0800

    Update Page.vue

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,21 +1,74 @@
 <template>
   <div>
-    
+    <div class="header_card my_card">
+        <el-button-group>
+            <el-button type="primary" @click="toPush('/')">返回工具集</el-button>
+            <el-button type="success" @click="openURL('https://wulvxinchen.cn/')">返回导航站</el-button>
+        </el-button-group>
+        <el-button type="primary" @click="toPush('/about')">关于</el-button>
+    </div>
+    <div class="header_card my_card">
+        <div class="header_card_logo">
+            <a href="./login" ><img src="https://my.wulvxinchen.cn/pictures/logo/logo.webp" alt="xinchen" class="header_card_img"></a>
+            <strong>心尘的工具集</strong>
+        </div>
+        <div class="header_card_info">
+            <span style="font-weight:600">更新，更美观，更现代化的心尘工具集！</span>
+        </div>
+    </div>
+    <div class="header_card my_card tool_card remove_flex" v-loading="loading">
+        <p class="tool_name"><strong>{{ tool.name }}</strong></p>
+        <p class="tool_type"><strong>数据库序号:{{ tool.id }}</strong></p>
+        <p class="tool_type"><strong>类型:{{ tool.type }}</strong></p>
+        <p class="tool_til">简介：{{ tool.til }}</p>
+        <p class="tool_url">网址：<a :href="tool.url" target="_blank">{{ tool.url }}</a></p>
+        <div>
+            <el-button type="primary" >分享</el-button>
+            <el-button type="success" @click="openURL(tool.url)">访问</el-button>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Page',
   components: {
   },
   data(){
     return{
-      
+        loading: false,
+        tool: {
+            "id": 1,
+            "type": "实用",
+            "name": "申论生成器",
+            "url": "https://sojo.im/slscq/",
+            "til": "在线生成申论"
+        }
     }
   },
   methods:{
-    
+    toPush(url){
+        this.$router.push(url)
+    },
+    openURL(url){
+        window.open(url, '_blank')
+    },
+    getTool(){
+        this.loading = true
+        axios.get('https://my.wulvxinchen.cn/tools2/api/searchID.php?id='+this.tool.id).then(res=>{
+            this.tool = res.data.data[0]
+            this.loading = false
+            // 最后删掉输出
+            console.log(this.tool)
+        })
+    }
+  },
+  mounted(){
+    this.tool.id = this.$route.params.id || this.$route.query.id;
+    this.getTool()
   }
 }
 </script>
