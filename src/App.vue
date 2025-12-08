fb2dae59dbda749118ce9459ed0fa3e4541cf385
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2025-12-09 00:03:49 +0800

    Update App.vue
    (oﾟ▽ﾟ)o

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@
 export default {
   name: 'App',
   components: {
-  }
+  },
+  mounted() {
+    console.log("欢迎使用心尘工具集！")
+    console.log("==============================")
+    console.log(" __        ___    __  ______ ")
+    console.log(" \\ \\      / / |   \\ \\/ / ___|")
+    console.log("  \\ \\ /\\ / /| |    \\  / |   ")
+    console.log("   \\ V  V / | |___ /  \\ |___ ")
+    console.log("    \\_/\\_/  |_____/_/\\_\\____|")
+    console.log("==============================")
+  },
 }
 </script>
