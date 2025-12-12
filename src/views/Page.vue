c53e19dec263ef6a171de68501fa140040f5ac50
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2025-12-13 02:58:15 +0800

    Update Page.vue
    实现功能：isvis设置为0的时候Page页面弹出提示

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -16,15 +16,22 @@
             <span style="font-weight:600">更新，更美观，更现代化的心尘工具集！</span>
         </div>
     </div>
+    <div class="header_card my_card tool_card remove_flex" v-if="tool.isvis == 0">
+      <el-alert title="警告Waring:不可访问或者限制展示!" type="warning" effect="dark" :closable="false" center show-icon
+      description="该工具已被管理员设置为无效工具或者访问受限,证明该工具在本工具集中已不被维护,强行访问该工具后果自负!"/>
+    </div>
     <div class="header_card my_card tool_card remove_flex" v-loading="loading" v-if="tool.id != 'ERROR'">
         <p class="tool_name"><strong>{{ tool.name }}</strong></p>
         <p class="tool_type"><strong>数据库序号:{{ tool.id }}</strong></p>
         <p class="tool_type"><strong>类型:{{ tool.type }}</strong></p>
         <p class="tool_til">简介：{{ tool.til }}</p>
-        <p class="tool_url">网址：<a :href="tool.url" target="_blank">{{ tool.url }}</a></p>
+        <p class="tool_url">
+          <span v-if="tool.isvis != 0">网址：<a :href="tool.url" target="_blank" >{{ tool.url }}</a></span>
+          <span v-else>该网址已不可访问</span>
+        </p>
         <div>
             <el-button type="primary" @click="shareURL(tool.id)">分享</el-button>
-            <el-button type="success" @click="openURL(tool.url)">访问</el-button>
+            <el-button type="success" @click="openURL(tool.url)" :disabled="tool.isvis == 0">访问</el-button>
         </div>
     </div>
     <div class="header_card my_card tool_card remove_flex" v-else>
@@ -119,7 +126,7 @@ export default {
               name: 'ERROR',
               url: 'ERROR',
               til: 'ERROR',
-              isvis: 0
+              isvis: -1
             }
             this.errorImg()
             ElMessage({
