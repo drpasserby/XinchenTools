06f0a44f27c15ca9213b70f2603e035bf6e9431b
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-02-22 00:10:19 +0800

    Update Page.vue
    优化样式

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -50,7 +50,7 @@
     </div>
     <el-dialog v-model="previewVisible" title="预览" align-center width="80%">
       <p>本窗口仅生成图片预览，内容仅供参考,请以实际情况为准</p>
-      <el-button type="primary" @click="openURL('https://cdn2.iocdn.cc/mshots/v1/' + tool.url)" >打开新窗口预览</el-button><br>
+      <el-button type="primary" @click="openURL('https://cdn2.iocdn.cc/mshots/v1/' + tool.url)" >打开新窗口预览</el-button><br><br>
       <div class="preview_container">
         <iframe 
           :src="'https://cdn2.iocdn.cc/mshots/v1/' + tool.url"
