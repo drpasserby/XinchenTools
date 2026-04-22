afce74cc6445ff4ff5cda5327eeee4e44f00fcac
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-04-23 04:06:13 +0800

    Update Login.vue
    添加加密算法

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -114,7 +114,15 @@ export default {
           message: '网络异常，请稍后再试',
           type: 'error'
         });
-      })
+      }
+    },
+    async hashPassword(password) {
+      const encoder = new TextEncoder();
+      const data = encoder.encode(password);
+      const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+      return hashHex;
     },
     generateCaptcha(){
       const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789';
